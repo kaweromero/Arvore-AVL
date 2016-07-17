54076f1897a64cd0a03a7626245dc2de6f7cdf04
kaweromero <kaweromero@gmail.com>
2016-07-17 15:19:24 -0300

    Uma arvore sapeca

---
+++ src/AvlNode.java
@@ -0,0 +1,15 @@
+
+    public class AvlNode {
+    	protected int height;       // Height
+    	protected int key;
+        protected AvlNode left, right;
+        public AvlNode ( int theElement ) {
+            this( theElement, null, null );
+        }
+        public AvlNode ( int theElement, AvlNode lt, AvlNode rt ) {
+            key = theElement;
+            left = lt;
+            right = rt;
+            height   = 0;
+        }
+    }
